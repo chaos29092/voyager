525a74fb2692e0cee116e0ea0d1ffcbee2f36a57
chaos <chaos29092@gmail.com>
2019-01-18 14:13:18 +0800

    update

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-09-29 08:21:02.
+    * Generated on 2019-01-18 06:12:30.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
